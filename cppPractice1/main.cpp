66060eb7f52840d94062459c9f0f7ddfe3902119
irokanade <michaeljs.leong@gmail.com>
2021-01-27 11:28:21 +0800

    Initial Commit

---
+++ cppPractice1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppPractice1
+//
+//  Created by Michael Leong on 2021/1/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
